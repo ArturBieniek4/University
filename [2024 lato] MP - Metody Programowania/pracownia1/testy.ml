f68af88051823a401b019e0be52032a8976a4440
Artur Bieniek <ar2rekb@gmail.com>
2025-03-26 23:08:12 +0100

    Delete unnecessary file

--- [2024 lato] MP - Metody Programowania/pracownia1/testy.ml
+++
@@ -1,9 +0,0 @@
-let rec choose m n =
-  if m > n then [] else m :: choose (m +1) n;;
-
-let ( let* ) xs ys = List . concat_map ys xs;;
-
-let two_num_product n m =
-  let* a = choose 1 n in
-  let* b = choose a n in
-  if a * b = m then [a , b] else []
